882990b72af159387e6cb26e3031a1a1bba45a61
Marko <illuminati3210@gmail.com>
2016-11-25 23:01:04 +0100

    Remove untracked files

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,3 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
-
-Route::get("/data", ["as" => "mt4_api", "uses" => "MT4Controller@api_Json"]);
-
